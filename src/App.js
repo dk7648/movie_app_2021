0321532213ed7554398cca807406dabd0920637c
dk7648 <76927860+dk7648@users.noreply.github.com>
2021-09-12 17:27:48 +0900

    ~ #2.4 commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import PropTypes from "prop-types"
+function Food({name, index, rating}) { // props.fav == {fav}
+  return <h1>I like {name}, index={index}, rating={rating}/5.0</h1>;
+}
 
+
+Food.propTypes = {
+name: PropTypes.string.isRequired,
+index: PropTypes.number.isRequired,
+rating: PropTypes.number.isRequired,
+}
+const foodILike = [ 
+{
+  id:1,
+    name: "Kimchi",
+    index: 1,
+    rating: 4.9,
+},
+{
+  id:2,
+  name: "Samgyeopsal",
+  index: 2,
+  rating: 4.8,
+}]
+
+function renderFood(dish) {
+  console.log(dish)
+  return <Food key={dish.id} name={dish.name} index={dish.index} rating={dish.rating}/>
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {foodILike.map(renderFood)}
     </div>
+    
   );
 }
 
