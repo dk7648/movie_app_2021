50f0efa37d5827dde3ac5c11124678b2110efdb3
dk7648 <76927860+dk7648@users.noreply.github.com>
2021-09-26 15:58:49 +0900

    ~ #5.0

--- src/App.js
+++ src/App.js
@@ -1,40 +1,84 @@
 import React from "react"
-import PropTypes from "prop-types"
-function Food({name, index, rating}) { // props.fav == {fav}
-  return <h1>I like {name}, index={index}, rating={rating}/5.0</h1>;
-}
+import axios from "axios"
+//import { object } from "prop-types";
+import Movie from "./Movie"
+import "./App.css"
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     console.log("hello");
+//   }
+//   state = {
+//     count: 0
+//   };
 
+//   add = () => {
+//     console.log("add");
+//     this.setState(current => ({count:current.count+1}))
+//   }
+//   minus = () => {
+//     console.log("minus");
+//     this.setState({count: this.state.count - 1})
+//   }
+//   componentDidMount() {
+//     console.log("component rendered");
+//   }
+//   componentDidUpdate() {
+//     console.log("I just updated");
+//   }
+//   componentWillUnmount() {
+//     console.log("Goodbye cruel world")
+//   }
+//   render(){
+//     console.log("i'm rendering");
+//     return <div>
+//       <h1>The number is {this.state.count}</h1>
+//       <button onClick={this.add}>Add</button>
+//       <button onClick={this.minus}>Minus</button>
+//       </div>
+//   }
+// }
 
-Food.propTypes = {
-name: PropTypes.string.isRequired,
-index: PropTypes.number.isRequired,
-rating: PropTypes.number.isRequired,
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const {isLoading, movies} = this.state;
+    return (
+    <section className = "container">
+      {isLoading ? 
+        (<div className="loader"><span className="loader__text">Loading...</span></div>) : 
+        <div className="movies">
+          {movies.map(movie => (
+            <Movie 
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      }
+    </section>
+    );
+    }
 }
-const foodILike = [ 
-{
-  id:1,
-    name: "Kimchi",
-    index: 1,
-    rating: 4.9,
-},
-{
-  id:2,
-  name: "Samgyeopsal",
-  index: 2,
-  rating: 4.8,
-}]
-
-function renderFood(dish) {
-  console.log(dish)
-  return <Food key={dish.id} name={dish.name} index={dish.index} rating={dish.rating}/>
-}
-function App() {
-  return (
-    <div>
-        {foodILike.map(renderFood)}
-    </div>
-    
-  );
-}
-
 export default App;
